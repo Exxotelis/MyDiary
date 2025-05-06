61c51813c4a8110ab8919a60326a525554f8bc70
Exxotelis <telis0kaps@gmail.com>
2025-05-06 20:40:25 +0100

    Temporary SuperUser

--- mydiary/urls.py
+++ mydiary/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from diary.views import create_admin
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('diary.urls')),
+    path('createsuperuser/', create_admin),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
